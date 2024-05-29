24f46776863589c6ef22a2f4844b285f7ab1cd08
anushaT-8065 <120164125+anushaT-8065@users.noreply.github.com>
2024-05-29 22:40:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+const port = 5001; 
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
